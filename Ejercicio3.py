29b1a64957b152b5b90725adf20733fbd37bb07e
Ruben Fernandez <ruben693@gmail.com>
2019-11-20 12:08:15 +0000

    Ejercicio 3 implementado

---
+++ Ejercicio3.py
@@ -0,0 +1,34 @@
+from random import *
+
+def genera_numero_aleatorio(minimo, maximo):
+
+  try:
+    if (minimo > maximo):
+      aux = minimo
+      minimo = maximo
+      maximo = aux
+    
+    return randint(minimo, maximo)
+  except TypeError:
+    print("Debes escribir números")
+    return -1
+
+def encuentra_numero_aleatorio(numero_generado):
+  encontrado = False
+  intentos = 0
+
+  while (not encontrado):
+    numero_usuario = int(input("Introduce un número: "))
+    
+    if (numero_usuario > numero_generado):
+      print("El numero que has introducido es mayor que el número generado. Sigue intentando.")
+      intentos+=1
+    elif (numero_usuario < numero_generado):
+      print("El numero que has introducido es menor que el número generado. Sigue intentando.")
+      intentos+=1
+    else:
+      print("NUMERO CORRECTO!")
+      print("\nLo has conseguido en: ", intentos, " intentos.")
+      encontrado = True
+
+encuentra_numero_aleatorio(genera_numero_aleatorio(1, 100))
