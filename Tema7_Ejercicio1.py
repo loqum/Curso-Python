7d18564fa42d1686fe5c2fd0553e6012ddb6d4cc
Ruben Fernandez <loqum@users.github.com>
2019-11-21 21:13:22 +0100

    Iniciado ejercicio 1 tema 7

--- Tema7_Ejercicio1.py
+++ Tema7_Ejercicio1.py
@@ -1,24 +1,19 @@
-class Persona:
-  nombre = ""
-  primer_apellido = ""
-  segundo_apellido = ""
+class Cliente:
+    nombre = ""
+    primer_apellido = ""
+    segundo_apellido = ""
 
-  def print_persona(self, nombre, primer_apellido, segundo_apellido):
-    print(self.nombre)
-    print(self.primer_apellido)
-    print(self.segundo_apellido)
-  
-  def __init__(self, nombre = "", primer_apellido = "", segundo_apellido = ""):
-    self.nombre = nombre
-    self.primer_apellido = primer_apellido
-    self.segundo_apellido = segundo_apellido
-  
-  def __str__(self):
-    return self.nombre
+    def __init__(self, nombre = "", primer_apellido = "", segundo_apellido = ""):
+        self.nombre = nombre;
+        self.primer_apellido = primer_apellido
+        self.segundo_apellido = segundo_apellido
 
-ruben = Persona()
-ruben.nombre = "Ruben"
-ruben.primer_apellido = "Fernandez"
-ruben.segundo_apellido = "Moreno" 
-ruben.print_persona(ruben.nombre, ruben.primer_apellido, ruben.segundo_apellido)
-jaime = Persona("Jaime", "Gutierrez", "Lozano")
+    def __str__(self):
+        return self.nombre
+
+    def print_cliente(self, nombre, primer_apellido, segundo_apellido):
+        print(self.nombre, self.primer_apellido, self.segundo_apellido)
+
+x = Cliente("Claudia", "García", "Pollastre")
+
+x.print_cliente(x.nombre, x.primer_apellido, x.segundo_apellido)
