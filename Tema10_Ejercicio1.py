a816f9c13b251f21e0faab0511613bdb9c0bc064
Ruben Fernandez <loqum@users.github.com>
2019-12-06 08:14:32 +0100

    Refactorizado

--- Tema10_Ejercicio1.py
+++ Tema10_Ejercicio1.py
@@ -1,15 +1,25 @@
 import shutil
 
-def escribir_leer_archivo:
-  file = open("C:/Users/rfernandezm/Desktop/fichero1.txt", "w")
-  file.write("Hola mundo")
-  print(file.read())
+PATH_DESKTOP = "C:/Users/ruben/Desktop/"
+PATH_DOCUMENTS = "C:/Users/ruben/Documents/"
+
+def escribir_leer_archivo(file_name, text):
+  file = open(PATH_DESKTOP + file_name, "w")
+  file.write(text)
   file.close()
 
-def sobreescribir_archivo:
-  file = open("C:/Users/rfernandezm/Desktop/fichero1.txt", "a")
-  file.write("Hola mundo.\n")
+def leer_archivo(file_name):
+    file = open(PATH_DESKTOP + file_name, "r")
+    print(file.read())
+
+def sobreescribir_archivo(file_name, text):
+  file = open(PATH_DESKTOP + file_name, "a")
+  file.write(text + "\n")
   file.close()
 
-def mover_archivo(url_file_original, url_file_destiny):
-  shutil.move("C:/Users/rfernandezm/Desktop/fichero1.txt", "C:/Users/rfernandezm/Documents/fichero1.txt")
+def mover_archivo(file_name, url_file_original, url_file_destiny):
+  shutil.move(url_file_original + file_name, url_file_destiny + file_name)
+
+escribir_leer_archivo("archivo.txt", "Hola mundo!")
+leer_archivo("archivo.txt")
+sobreescribir_archivo("archivo.txt", "Hola, vecino")
--- Tema10_Ejercicio3.py
+++ Tema10_Ejercicio3.py
@@ -1,9 +1,10 @@
 import urllib.request
 
+PATH_DESKTOP = "C:/Users/ruben/Desktop/"
+
 peticion = urllib.request.Request("http://www.google.com")
 respuesta = urllib.request.urlopen(peticion)
 pagina = respuesta.read()
-file = open("C:/Users/rfernandezm/Desktop/fichero1.txt", "bw")
+file = open(PATH_DESKTOP + "fichero1.txt", "bw")
 file.write(pagina)
 file.close()
-
